4697d203065e90926af047cf164497592155cecf
Eloy Duran <eloy.de.enige@gmail.com>
2010-10-09 17:04:33 +0200

    Create svn fixture repo

--- lib/git-svn-mirror.rb
+++ lib/git-svn-mirror.rb
@@ -1,2 +1,17 @@
+require "optparse"
+
 class GitSVNMirror
+  attr_reader :from, :to, :workbench
+
+  def init(argv)
+    OptionParser.new do |opt|
+      opt.on('--from URI', 'The location of the SVN repository that is to be mirrored.') { |uri| @from = uri }
+      opt.on('--to URI',   'The location of the GIT repository that is the mirror.')     { |uri| @to = uri }
+    end.parse!(argv)
+    @workbench = argv.shift
+  end
+
+  def svn_repo_name
+    File.basename(@from)
+  end
 end
--- spec/git-svn-mirror_spec.rb
+++ spec/git-svn-mirror_spec.rb
@@ -1,11 +1,39 @@
 require File.expand_path("../spec_helper", __FILE__)
 
 describe "GitSVNMirror, concerning `init'" do
-  before do
-    @mirror = GitSVNMirror.new
+
+  # before all
+  FileUtils.mkdir_p(WORKBENCH_REPO)
+  @mirror = GitSVNMirror.new
+  @mirror.init(%W{ --from=file://#{SVN_REPO} --to=#{GIT_REPO} #{WORKBENCH_REPO}})
+
+  it "parses the given arguments" do
+    @mirror.from.should == "file://#{SVN_REPO}"
+    @mirror.to.should == GIT_REPO
+    @mirror.workbench.should == WORKBENCH_REPO
   end
 
-  it "does something" do
-    @mirror.init(%w{})
+  it "returns the name of the SVN repo" do
+    @mirror.svn_repo_name.should == File.basename(SVN_REPO)
+  end
+
+  it "creates the GIT config" do
+    config = File.read(File.join(WORKBENCH_REPO, 'config'))
+    config.should == <<EOS
+[core]
+	repositoryformatversion = 0
+	filemode = true
+	bare = true
+	ignorecase = true
+	autocrlf = false
+[svn-remote "svn"]
+	url = #{SVN_REPO}
+	fetch = #{@mirror.svn_repo_name}/trunk:refs/remotes/trunk
+	branches = #{@mirror.svn_repo_name}/branches/*:refs/remotes/*
+	tags = #{@mirror.svn_repo_name}/tags/*:refs/remotes/tags/*
+[remote "origin"]
+	url = #{GIT_REPO}
+	fetch = +refs/heads/*:refs/remotes/origin/*
+EOS
   end
 end
