df26e12d741a9781de547ef0ded401cd6726a243
Eloy Duran <eloy.de.enige@gmail.com>
2010-10-10 04:28:07 +0200

    Print init and update banners when needed.

--- lib/git-svn-mirror.rb
+++ lib/git-svn-mirror.rb
@@ -1,4 +1,3 @@
-require "optparse"
 
 class GitSVNMirror
   attr_accessor :from, :to, :workbench, :authors_file
@@ -6,27 +5,50 @@ class GitSVNMirror
   def self.run(argv)
     mirror = new
     case argv.shift
-    when 'init'
-      OptionParser.new do |opt|
-        opt.on('--from URI', 'The location of the SVN repository that is to be mirrored.') { |uri| mirror.from = uri }
-        opt.on('--to URI',   'The location of the GIT repository that is the mirror.') { |uri| mirror.to = uri }
-        opt.on('--workbench PATH', 'The location of the workbench repository from where the mirroring will occur') { |wb| mirror.workbench = wb }
-        opt.on('--authors-file PATH', 'An optional authors file used to migrate SVN usernames to GIT’s format') { |af| mirror.authors_file = af }
-      end.parse!(argv)
+    when 'init'   then init(mirror, argv)
+    when 'update' then update(mirror, argv)
+    end
+    mirror
+  end
+
+  def self.init(mirror, argv)
+    require 'optparse'
+
+    opts = OptionParser.new do |o|
+      o.banner = "Usage: git-svn-mirror init [mandatory options]"
+      o.separator "\n  Mandatory options are --from, --to, and --workbench.\n\n"
+      o.on('--from URI',          'The location of the SVN repository that is to be mirrored.')                    { |uri| mirror.from = uri }
+      o.on('--to URI',            'The location of the GIT repository that is the mirror.')                        { |uri| mirror.to = uri }
+      o.on('--workbench PATH',    'The location of the workbench repository from where the mirroring will occur.') { |wb|  mirror.workbench = wb }
+      o.on('--authors-file PATH', 'An optional authors file used to migrate SVN usernames to GIT\'s format.')      { |af|  mirror.authors_file = af }
+    end
+    opts.parse!(argv)
+
+    if mirror.from && mirror.to && mirror.workbench
       mirror.init
-    when 'update'
+    else
+      puts opts
+    end
+  end
+
+  def self.update(mirror, argv)
+    if argv.empty? || argv.include?('--help')
+      puts "Usage: git-svn-mirror update [workbench1] [workbench2] ..."
+      puts "\n  At least one workbench path is required."
+    else
       argv.each do |workbench|
         mirror.workbench = workbench
         mirror.update
       end
     end
-    mirror
   end
 
   def init
     sh "git init --bare"
+
     sh "git svn init --stdlayout --prefix=svn/ #{@from}"
     sh "git config --add svn-remote.svn.authorsfile '#{@authors_file}'" if @authors_file
+
     sh "git remote add origin #{@to}"
     sh "git config --add remote.origin.push 'refs/remotes/svn/*:refs/heads/*'"
 
